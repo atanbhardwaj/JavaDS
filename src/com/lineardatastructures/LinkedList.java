a6b34d596d76fce1d05ff6362cd7034a11bb75bc
Atan Bhardwaj <atanbhardwaj@gmail.com>
2021-02-12 16:42:34 +0530

    Added LinkedList Class

---
+++ src/com/lineardatastructures/LinkedList.java
@@ -0,0 +1,139 @@
+package com.lineardatastructures;
+
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+    private class Node{
+        private int value;
+        private Node next;
+        public Node(int value){
+            this.value = value;
+        }
+    }
+    private Node first;
+    private Node last;
+    private int size;
+
+    public void addFirst(int item){
+        Node node = new Node(item);
+        if(isEmpty())
+            first = last = node;
+        else {
+            node.next = first;
+            first = node;
+        }
+        size++;
+    }
+
+    public void removeFirst(){
+        if(isEmpty())
+            throw new NoSuchElementException();
+
+        if(first==last)
+            first = last = null;
+        else
+        {
+            Node temp = first.next;
+            first.next = null;
+            first = temp;
+        }
+        size--;
+    }
+
+    public void addLast(int item){
+        Node node = new Node(item);
+        if( isEmpty() )
+            first = last = node;
+        else
+            last = last.next = node;
+        size++;
+    }
+
+    public void removeLast(){
+        if(isEmpty())
+            throw new NoSuchElementException();
+        if(first==last)
+            first = last = null;
+        else{
+            Node previous = getPrevious(last);
+            last = previous;
+            last.next = null;
+        }
+        size--;
+    }
+
+    private Node getPrevious(Node node){
+        Node current = first;
+        while(current!=null){
+            if(current.next == node)    return current;
+            current = current.next;
+        }
+        return null;
+    }
+
+    private boolean isEmpty(){
+        return first==null;
+    }
+
+    public int indexOf(int item){
+        int index = 0;
+        Node current = first;
+        while( current != null ){
+            if(current.value==item)
+                return index;
+            index++;
+            current = current.next;
+        }
+        return -1;
+    }
+
+    public boolean contains(int item){
+        return indexOf(item) != -1;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public int[] toArray(){
+        int[] array = new int[size];
+        Node current = first;
+        int index = 0;
+        while ( current != null ){
+            array[index++] = current.value;
+            current = current.next;
+        }
+        return array;
+    }
+
+    public void reverse(){
+        if(isEmpty()) return;
+        Node p = first,q = null,r = null;
+        while(p!=null){
+            r = q;
+            q = p;
+            p = p.next;
+            q.next = r;
+        }
+        last = first;
+        last.next = null;
+        first = q;
+    }
+
+    public int getKthFromTheEnd(int k){
+        if(isEmpty()) throw new IllegalStateException();
+        Node apoint = first;
+        Node bpoint = first;
+        while((k-1)>0){
+            bpoint = bpoint.next;
+            if(bpoint == null)  throw new IllegalArgumentException();
+            k--;
+        }
+        while(bpoint!=last){
+            apoint = apoint.next;
+            bpoint = bpoint.next;
+        }
+        return apoint.value;
+    }
+
+}
--- src/com/lineardatastructures/Main.java
+++ src/com/lineardatastructures/Main.java
@@ -3,13 +3,35 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        Array array = new Array(3);
-        array.insert(1);
-        array.insert(2);
-        array.insert(3);
-        array.insert(4);
-        array.insert(5);
-        array.print();
-
+//        Array array = new Array(3);
+//        array.insert(1);
+//        array.insert(2);
+//        array.insert(3);
+//        array.insert(4);
+//        array.insert(5);
+//        array.print();
+//        array.removeAt(3);
+//        array.print();
+//        System.out.println(array.indexOf(5));
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addFirst(5);
+        list.addLast(50);
+//        System.out.println("Size " + (list.size()));
+//        int x = list.indexOf(10);
+//        System.out.println(x);
+//        System.out.println(list.contains(10));
+//        System.out.println(list.contains(500));
+//        list.removeFirst();
+//        list.removeLast();
+//        list.removeLast();
+//        list.removeLast();
+        System.out.println("Size " + (list.size()));
+        list.reverse();
+        int[] array = list.toArray();
+        System.out.println(Arrays.toString(array));
+        System.out.println(list.getKthFromTheEnd(-1));
     }
 }
