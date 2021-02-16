0b6e359482b7432e160928039da363b9bdc9c148
Atan Bhardwaj <atanbhardwaj@gmail.com>
2021-02-16 22:26:43 +0530

    Added Heap Class

---
+++ src/com/nonlineardatastructures/Heap.java
@@ -0,0 +1,14 @@
+package com.nonlineardatastructures;
+
+public class Heap {
+    private int []items = new int[10];
+    private int size;
+    public void insert(int value)
+    {
+        items[size++] = value;
+    }
+
+
+
+
+}
